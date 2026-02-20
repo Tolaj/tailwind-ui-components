6c2eeaa10c31b55cf3f175d69c0d24ae3adbc6f8
Tolaj <swapnilhgf@gmail.com>
2026-02-20 04:01:41 -0500

    fix1

--- src/config/mongoConnection.js
+++ src/config/mongoConnection.js
@@ -49,33 +49,30 @@
 
 // export default connectDB;
 
-
+// lib/mongodb.js
 import mongoose from "mongoose";
-import { mongoConfig } from "./settings.js";
+import { mongoConfig } from './settings.js';
 
 let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+export default async function connectDB() {
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(mongoConfig.serverUrl).then((mongoose) => {
-      console.log("✅ MongoDB connected");
+    cached.promise = mongoose.connect(mongoConfig.serverUrl, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    }).then((mongoose) => {
+      console.log(`✅ MongoDB Connected: ${mongoose.connection.host}`);
       return mongoose;
+    }).catch((err) => {
+      console.error("❌ MongoDB connection error:", err);
+      throw err;
     });
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
 }
-
-export default connectDB;
